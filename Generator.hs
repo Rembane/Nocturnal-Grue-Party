938d3315ac4fac33ae68f2ef146f7cc6f8281468
Rembane <andeke@gmail.com>
2013-11-19 16:15:34 +0100

    Fixed some bugs and made the code prettier.
    I was too nice and let things start living when they only should
    have survived.

--- Generator.hs
+++ Generator.hs
@@ -1,7 +1,7 @@
 module Generator where
 
 import Control.Monad (join)
-import Data.Maybe (catMaybes)
+import Data.Maybe (catMaybes, fromMaybe)
 import Data.List (intersperse)
 import qualified Data.Vector as V
 import System.Environment (getArgs)
@@ -35,21 +35,24 @@ randomLabyrinth rg height width = V.fromList $ map (V.fromList) $ take height $ 
 -- Determine if a cell will be born, survive or die.
 -- True = It's alive!
 -- TODO: Use a set for survive
--- survival :: (Int, [Int]) -> Int -> Int -> Labyrinth -> Bool
-survival (born, survive) y x labyrinth = survival' $ length $ filter (== Wall) $ catMaybes $ map (\(dy, dx) -> join $ cell (dy + y) (dx + x) labyrinth) surroundings
+survival :: (Int, [Int]) -> Int -> Int -> Labyrinth -> Bool
+survival (born, survive) y x labyrinth = survival' $ length $ filter (== Wall) $ catMaybes $ map (\(dy, dx) -> cell (dy + y) (dx + x) labyrinth) surroundings
     where
-        survival' n    = (born == n) || (n `elem` survive)
+        survival' n    = (born == n) || (fromMaybe False $ fmap (== Wall) (cell y x labyrinth)) && (n `elem` survive)
         surroundings   = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)] -- 9x9 (0, 0), 
         row y' lab     = lab V.!? y'
-        cell y' x' lab = fmap (row x') (row y' lab)
+        cell y' x' lab = join $ fmap (row x') (row y' lab)
 
 boolToCell :: Bool -> LBasic
 boolToCell False = Empty
 boolToCell True  = Wall
 
 -- Pass it a labyrinth, some rules and you get another one back. :D
-maze :: (Int, [Int]) -> Int -> Int -> Labyrinth -> Labyrinth
-maze (born, survive) height width labyrinth = V.fromList $ map (\y -> V.fromList $ map (\x -> boolToCell $ survival (born, survive) y x labyrinth) [0..height-1]) [0..width-1]
+maze :: (Int, [Int]) -> Labyrinth -> Labyrinth
+maze (born, survive) labyrinth = V.fromList $ map (\y -> V.fromList $ map (\x -> boolToCell $ survival (born, survive) y x labyrinth) [0..height-1]) [0..width-1]
+    where 
+        height = V.length labyrinth
+        width  = V.length $ labyrinth V.! 0
 
 mazeLoop :: Int -> (Labyrinth -> Labyrinth) -> Labyrinth -> Labyrinth
 mazeLoop 0 _ lab = lab
@@ -67,4 +70,4 @@ main = do
     args <- getArgs
     rg   <- initialize
     let iterations = read $ head args
-    putStrLn $ labyrinthToStr $ mazeLoop iterations (maze (3, [1,2,3,4,5]) 20 20) (randomLabyrinth rg 20 20)
+    putStrLn $ labyrinthToStr $ mazeLoop iterations (maze (3, [1,2,3,4,5])) (randomLabyrinth rg 20 20)
